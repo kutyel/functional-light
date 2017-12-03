e74d9bd84342fc86514dcaccb513f9cdea5be647
kutyel <flaviocorpa@gmail.com>
2017-12-03 19:56:38 +0100

    Solve Exercise 7

---
+++ exercises/ex7/ex7.js
@@ -0,0 +1,22 @@
+// const one = () => 1
+// const two = () => 2
+const f = x => () => x
+
+const add = (x, y) => x + y
+
+// console.log(add(one(), two()))
+
+const add2 = (f, g) => add(f(), g())
+
+const addn = fns => fns.reduce((f, g) => () => add2(f, g))()
+
+const data = [10, 100, 30, 100, 42, 10, 15]
+
+const isEven = x => x % 2 === 0
+
+const fns = data
+  .reduce((arr, x) => (!arr.includes(x) ? [...arr, x] : arr), [])
+  .filter(isEven)
+  .map(f)
+
+console.log(addn(fns)) // 182
